04ab7b1a94e1c4e3b82dc3d5b98887166ef83d82
eriksalinas <erik61869@gmail.com>
2022-06-20 13:23:45 -0300

    cambio

--- sistema_ventas/tipoproducto-listado.php
+++ sistema_ventas/tipoproducto-listado.php
@@ -16,7 +16,7 @@ include_once("header.php");
           <h1 class="h3 mb-4 text-gray-800">Listado de productos</h1>
           <div class="row">
                 <div class="col-12 mb-3">
-                    <a href="cliente-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
+                    <a href="tipoproducto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                 </div>
             </div>
           <table class="table table-hover border">
